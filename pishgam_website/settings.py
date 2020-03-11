7be9eb7b8ac89fc5f3c5f28c05c186db4d14ba86
bardia dorry <bardiadorry@gmail.com>
2020-03-11 23:16:39 +0330

    for postgres

--- pishgam_website/settings.py
+++ pishgam_website/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'pishgam_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tbsh981221',
+        'USER': 'tbsh',
+        'PASSWORD': 'Tabesh1378Pishgam',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
