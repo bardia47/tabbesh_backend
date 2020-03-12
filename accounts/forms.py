d29e86a49403401d306f6ff17521a0080872103f
bardia dorry <bardiadorry@gmail.com>
2020-03-12 23:59:37 +0330

    for new user

---
+++ accounts/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import City, User
+
+class UserForm(forms.ModelForm):
+    username = forms.CharField(max_length=30)
+    password = forms.CharField(max_length=30)
+    email = forms.EmailField
+    city = forms.ModelChoiceField(queryset=City.objects.all())
+    
+    class Meta:
+        model = User
+        fields = ('username','password','email','city')
