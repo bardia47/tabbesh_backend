7c3dfe30ce07aadee5b8598447a64ccb36e6e7f0
bardia dorry <bardiadorry@gmail.com>
2020-03-12 23:59:05 +0330

    for new user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Role
+from .models import User
+
 
 # Register your models here.
 
-admin.site.register(Profile)
+admin.site.register(User)
+admin.site.register(Role)
