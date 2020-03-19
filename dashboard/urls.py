09462b2ff810304080012707bbefd4ab20846ac0
bardia dorry <bardiadorry@gmail.com>
2020-03-19 01:52:18 +0000

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path 
+
 from .views import dashboard , edit_profile , lessons
+from django.conf import settings 
+from django.conf.urls.static import static 
+
+
 urlpatterns = [
     path('' , dashboard , name='dashboard'),
-    path('lessons' , lessons , name="lessons" ),
-    path('edit_profile' , edit_profile , name="edit_profile" ),
+    path('lessons' , lessons , name="lessons"),
+    path('edit_profile' , edit_profile , name="edit_profile"),
 ]
+
+if settings.DEBUG: 
+        urlpatterns += static(settings.MEDIA_URL, 
+                              document_root=settings.MEDIA_ROOT)
