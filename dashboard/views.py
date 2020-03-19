ba4f8e063d6a1c0285c87fb8ec0dd1a647cd1dde
bardia dorry <bardiadorry@gmail.com>
2020-03-19 01:53:53 +0000

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,27 +1,32 @@
 from django.shortcuts import render , redirect
 from accounts.models import User
+
 from .forms import ProfileForm
 
+
 # Create your views here.
-
-
 def dashboard(request):
     return render(request , 'dashboard/dashboard.html')
+
 
 # Edit Profile Page 
 def edit_profile(request):
     if request.method == 'POST':
-        form = ProfileForm(data=request.POST , instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect ('dashboard')
+        if request.POST.get("upload"):
+            
+            form = ProfileForm(data=request.POST , instance=request.user)
+            if form.is_valid():
+                form.save()
+                return redirect ('dashboard')
         else:
             form = ProfileForm() 
-            return render(request , 'dashboard/profile_page.html' , {'error' : "خطا در ثبت نام" , 'form':form})
+            form.errors = {'username' : "خطا در ثبت نام" } 
+            return render(request , 'dashboard/profile_page.html' , {'form':form})
     else:  
-        form = ProfileForm()  
+        form = ProfileForm()
         return render(request , 'dashboard/profile_page.html' , { 'form':form })
 
-#Lessons Page
+
+# Lessons Page
 def lessons(request):
     return render (request , 'dashboard/lessons.html')
