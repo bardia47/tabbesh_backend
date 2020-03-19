51459a11fae72015e9a34c476ce08b4c9cd23ed4
bardia dorry <bardiadorry@gmail.com>
2020-03-19 01:51:13 +0000

    Update forms.py

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,11 +1,12 @@
-from django import forms
 from accounts.models import User , City , Grade 
+from django import forms
+
 
 class ProfileForm(forms.ModelForm):
 
-    city = forms.ModelChoiceField(queryset=City.objects.all(), required=True )
-    grades = forms.ModelMultipleChoiceField(queryset=Grade.objects.all(), required=True )
-    avatar = forms.ImageField(required=False)
+    city = forms.ModelChoiceField(queryset=City.objects.all(), required=True)
+    grades = forms.ModelMultipleChoiceField(queryset=Grade.objects.all(), required=True)
+    #avatar = forms.ImageField(required=False)
     first_name = forms.CharField(max_length=30 , required=True)
     last_name = forms.CharField(max_length=30 , required=True)
     national_code = forms.CharField(max_length=10 , required=True)
@@ -15,4 +16,4 @@ class ProfileForm(forms.ModelForm):
 
     class Meta :
         model = User
-        fields = ('city','grades','avatar','first_name' , 'last_name' , 'national_code' , 'address','gender','phone_number')
+        fields = ('city', 'grades', 'avatar', 'first_name' , 'last_name' , 'national_code' , 'address', 'gender', 'phone_number')
