a0720edde62cab861e9dbb81838fc9cafc8523db
Amirali <amiralifmj@gmail.com>
2020-03-21 03:54:09 +0430

    modify models.py and some polish

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,29 +1,32 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from accounts.models import User
+import datetime
 
 from .forms import ProfileForm
 
 
 # Create your views here.
 def dashboard(request):
-    return render(request , 'dashboard/dashboard.html')
+    today = datetime.datetime.now()
+    # user = get_object_or_404(User, pk=request.user.id)
+    return render(request, 'dashboard/dashboard.html', {'today': today})
 
 
 # Edit Profile Page 
 def edit_profile(request):
     if request.method == 'POST':
-        form = ProfileForm(data=request.POST , instance=request.user)
+        form = ProfileForm(data=request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect ('dashboard')
+            return redirect('dashboard')
         else:
-            form = ProfileForm() 
-            return render(request , 'dashboard/profile_page.html' , {'form':form , 'error':'خطا در ثبت نام'})
-    else:  
+            form = ProfileForm()
+            return render(request, 'dashboard/profile_page.html', {'form': form, 'error': 'خطا در ثبت نام'})
+    else:
         form = ProfileForm()
-        return render(request , 'dashboard/profile_page.html' , { 'form':form })
+        return render(request, 'dashboard/profile_page.html', {'form': form})
 
 
 # Lessons Page
 def lessons(request):
-    return render (request , 'dashboard/lessons.html')
+    return render(request, 'dashboard/lessons.html')
