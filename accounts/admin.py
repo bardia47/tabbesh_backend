520a5f1aeaddf23696de73a52dc3f8c3ab8cb24f
bardia dorry <bardiadorry@gmail.com>
2020-03-24 21:17:22 +0000

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,6 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django import forms
 from django.contrib.auth.hashers import make_password
 from .models import *
-from rest_framework.fields import ReadOnlyField
 
 
 class UserCreationForm(forms.ModelForm):
