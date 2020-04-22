dab388d801779d0fb800d383281ea39f71c87f71
Ehsan Rezaee <ehsanrezaee29@gmail.com>
2020-04-22 13:35:03 +0430

    fix edit profile page width

--- pishgam_website/settings.py
+++ pishgam_website/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'nan_@dp+&g%75ke-ac%yn(8@2su8r9fn0!-hlzdmz1ia0!-f$x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['tabbesh.ir', '185.4.31.70']
+ALLOWED_HOSTS = ['*']
 
 LOGGING = {
     'version': 1,
