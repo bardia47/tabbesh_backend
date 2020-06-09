50a058a4b31d5a1ecbad8c60b6543b16d912aeef
Ehsan Rezaee <ehsanrezaee29@gmail.com>
2020-06-09 19:34:23 +0430

    Version 3 of paginations

--- dashboard/static/dashboard/js/lessons.js
+++ dashboard/static/dashboard/js/lessons.js
@@ -1,39 +1,61 @@
-// function for get get variable in js
-function getUrlParameter(sParam) {
-    let sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-    }
+// function for get Request.GET variable
+function getUrlParameter(url, param) {
+    urlArray = url.split("/")
+    paramets = urlArray[urlArray.length - 1]
+    const urlParams = new URLSearchParams(paramets);
+    return urlParams.get(param)
 }
 
-//get lessons with ajax
+// hostname of project -- example : https://127.0.0.1:8000
+let arrayHref = window.location.href.split("/")
+let hostName = arrayHref[0] + "//" + arrayHref[2]
+
+// first pagination when user request https://127.0.0.1:8000/dashboard/lessons/
 $(function () {
+    pagination()
+})
+
+// pagination when user return to previous page --> hint: read about history javascript stack
+window.onpopstate = function (event) {
+    pagination(event.state.url)
+    console.log(history);
+};
+
+//get lessons with ajax
+function pagination(url) {
+    // check with page go
+    if (url) {
+        urlAjax = url
+    } else {
+        if (getUrlParameter(window.location.href, "page")) {
+            urlAjax = hostName + "/dashboard/test/?page=" + getUrlParameter(window.location.href, "page")
+        } else {
+            urlAjax = hostName + "/dashboard/test/?page=1"
+            history.replaceState({url: urlAjax}, null, window.location.href + "?page=1")
+        }
+    }
+
+    // get JSON and Response Header
     $.ajax({
-        url: "http://127.0.0.1:8000/dashboard/test/",
+        url: urlAjax,
         type: "GET",
         success: function (courseCards, textStatus, request) {
+            $(".card-group").empty()
+            $(".pagination").empty()
             renderLessenCards(courseCards)
             renderPagination(request.getResponseHeader('Last-Page'))
+            console.log(urlAjax)
         },
         error: function () {
             alert("خطا در بارگزاری دروس ... لطفا دوباره امتحان کنید!")
         },
     });
-});
+}
 
-// hostname of project -- example : https://127.0.0.1:8000
-let hostName = window.location.href.split("/")[0]
-
+// Add course card to div card group
 function renderLessenCards(courseCards) {
     $.each(courseCards, function (index, courseCard) {
+        // parse Date to ISO date format and use persianDate jQuery
         let endDateCourse = new persianDate(Date.parse(courseCard.end_date))
         let nextClassDate = new persianDate(Date.parse(courseCard.first_class))
         // check class is active or not
@@ -65,12 +87,12 @@ function renderLessenCards(courseCards) {
                     <h4 class="card-title">${courseCard.title}</h4>
                     <!-- Course teacher  -->
                     <div class="course-teacher-name">
-                        <h6>استاد {{ course.teacher.get_full_name }}</h6>
+                        <h6>استاد ${courseCard.teacher}</h6>
                     </div>
                     <!-- Course calender for next class  -->
                     <div class="course-calender">
                         <p>
-                            <img src="{% static 'home/images/icons/clock.svg' %}" alt="">
+                            <img src="${hostName}/static/home/images/icons/clock.svg" alt="">
                             جلسه ی بعدی:
                             <span>
                                 ${endDateCourse.format("dddd")}
@@ -115,23 +137,25 @@ function renderLessenCards(courseCards) {
         `
         $(".card-group").append(lessonCardTemplate)
     });
+    $(".card-group").show()
 }
 
-
+// make pagination numbers
 function renderPagination(pageNumber) {
-    $('.pagination').append(`<a class="next page-numbers" href="${hostName}/"></a>"`)
-    for (let i = 1; i <= pageNumber; i++) {
-        $('.pagination').append(`<a class="page-numbers" href="javascript:;">${i}</a>`)
+    // $('.pagination').append(`<a class="next page-numbers" href="${hostName}/dashboard/test/"> Prev </a>`)
+    for (let number = 1; number <= pageNumber; number++) {
+        if (getUrlParameter(window.location.href, "page") == number) {
+            $('.pagination').append(`<span aria-current="page" class="page-numbers current">${number}</span>`)
+        } else {
+            $('.pagination').append(`<a class="page-numbers" href="${hostName}/dashboard/test/?page=${number}">${number}</a>`)
+        }
     }
-    $('.pagination').append(`<a class="next page-numbers" href="javascript:;">بعدی</a>`)
+    $(".page-numbers").click(function (event) {
+        event.preventDefault();
+        history.pushState({url: $(this).attr("href")}, null, "?page=" + getUrlParameter($(this).attr("href"), "page"));
+        pagination($(this).attr("href"))
+    });
+    // $('.pagination').append(`<a class="next page-numbers" href="${hostName}"> Next </a>`)
+    $(".pagination-wrapper").show()
+    $("html, body").animate({scrollTop: 0}, "slow");
 }
-
-
-pageNumberTemplate = `
-                <a class="page-numbers" href="javascript:;">1</a>
-                <a class="page-numbers" href="javascript:;">2</a>
-                <span aria-current="page" class="page-numbers current">3</span>
-                <a class="page-numbers" href="javascript:;">4</a>
-                <a class="page-numbers" href="javascript:;">5</a>
-                <a class="next page-numbers" href="javascript:;">بعدی</a>
-`
