b8a0a25f555d2aee7ffd36cc5c8f9f3b6e3299bb
Ehsan Rezaee <ehsanrezaee29@gmail.com>
2020-06-10 15:35:16 +0430

    add scheduler to website

---
+++ accounts/scheduler.py
@@ -0,0 +1,28 @@
+import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def start():
+    scheduler = BackgroundScheduler()
+    # read about cron in apscheduler document
+    # --> https://apscheduler.readthedocs.io/en/stable/modules/triggers/cron.html
+    scheduler.add_job(update_course_calenders, 'cron', hour=15, minute=34)
+    scheduler.start()
+
+
+def update_course_calenders():
+    from .models import Course, Course_Calendar
+    # we cant import in top because account app not register when this code run ...
+    now = datetime.datetime.now()
+    courses = Course.objects.filter(end_date__gt=now)
+    classes = Course_Calendar.objects.filter(course__in=courses)
+    # update all classes time
+    for class_course_calender in classes:
+        while class_course_calender.end_date < now:
+            print(class_course_calender.end_date)
+            class_course_calender.start_date += datetime.timedelta(days=7)
+            class_course_calender.end_date += datetime.timedelta(days=7)
+            class_course_calender.save()
+    # print log when course calender update ...
+    print("course calenders update at " + str(now))
+
--- dashboard/apps.py
+++ dashboard/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from . import scheduler
 
 
 class DashboardConfig(AppConfig):
     name = 'dashboard'
+
+    # run scheduler when app is ready
+    def ready(self):
+        scheduler.start()
