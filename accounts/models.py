da57c8bdef73dd6ea481835742c2159ca0edb01e
bardiadorry <bardiadorry@gmail.com>
2020-08-21 18:27:26 +0430

    debug create superUser

--- accounts/models.py
+++ accounts/models.py
@@ -52,7 +52,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = [ 'first_name', 'last_name']
+    REQUIRED_FIELDS = [ 'first_name', 'last_name','phone_number']
 
     def __str__(self):
         return self.get_full_name()
